0c8fe800fd21f478e229d6067a1b945227327d3d
arhgreen <aaron.hgreen1@outlook.com>
2025-04-22 15:13:48 +0000

    +

--- RawData/data_prep.R
+++ RawData/data_prep.R
@@ -5,12 +5,12 @@ library(readxl) # For reading in excel data
 
 #Data Preparation and manipulation----------------------------------------------
 ##imporing data----------------------------------------------------------------
-skyldig_kön_ålder_socialekonomisk_status <- read_excel("~/Documents/Dansk_Kriminalitet/RawData/Danmark_stats_crime_socioeconomic.xlsx", 1)
-skyldig_kön_ålder_ursprung <- read_excel("~/Documents/Dansk_Kriminalitet/RawData/Danmark_stats_crime_socioeconomic.xlsx", 2)
-anmälda_åtalade_region <- read_excel("~/Documents/Dansk_Kriminalitet/RawData/Danmark_stats_crime_socioeconomic.xlsx", 3)
-bidrag_ursprung_kön <- read_excel("~/Documents/Dansk_Kriminalitet/RawData/Danmark_stats_crime_socioeconomic.xlsx", 4)
-häktade_kön_ålder_utbildning <- read_excel("~/Documents/Dansk_Kriminalitet/RawData/Danmark_stats_crime_socioeconomic.xlsx", 5)
-arbetslösa_ålder_kön_region <- read_excel("~/Documents/Dansk_Kriminalitet/RawData/Danmark_stats_crime_socioeconomic.xlsx", 6)
+skyldig_kön_ålder_socialekonomisk_status <- read_excel("RawData/Danmark_stats_crime_socioeconomic.xlsx", 1)
+skyldig_kön_ålder_ursprung <- read_excel("RawData/Danmark_stats_crime_socioeconomic.xlsx", 2)
+anmälda_åtalade_region <- read_excel("RawData/Danmark_stats_crime_socioeconomic.xlsx", 3)
+bidrag_ursprung_kön <- read_excel("RawData/Danmark_stats_crime_socioeconomic.xlsx", 4)
+häktade_kön_ålder_utbildning <- read_excel("RawData/Danmark_stats_crime_socioeconomic.xlsx", 5)
+arbetslösa_ålder_kön_region <- read_excel("RawData/Danmark_stats_crime_socioeconomic.xlsx", 6)
 # Formating variables-----------------------------------------------------------
 
 ## SOCIOECONOMIC----------------------------------------------------------------
